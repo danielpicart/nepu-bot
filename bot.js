cdb23fc9dba9eda0254f39d24af79fa78c9f8efc
Nep :3 <rouant@users.noreply.github.com>
2018-03-23 20:00:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,6 +37,7 @@ client.on('message', message => {
       message.member.voiceChannel.join()
         .then(connection => { // Connection is an instance of VoiceConnection
           message.reply('NEPU ! Me voici !');
+          const dispatcher = connection.playFile('/song/');
         })
         .catch(console.log);
     } else {
